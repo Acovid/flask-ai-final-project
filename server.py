f979caaa48b5ab61bdc1a3a52eafbb3cbe3c78a0
Aco Vidovic <acovid@gmail.com>
2025-02-12 18:09:47 +0100

    finish task 6

--- server.py
+++ server.py
@@ -3,25 +3,20 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
 
 @app.route("/emotionDetector")
-def em_detect():
+def sent_analyzer():
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
 
-    # Extract the label and score from the response
-    # label = response['label']
-    # score = response['score']
-
-    # Return a formatted string with the sentiment label and score
-    # return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)    
+    # Return a dictionary with emotions
     return response
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
